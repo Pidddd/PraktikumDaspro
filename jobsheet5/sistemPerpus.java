901e7845cb5abf00b6f0b392152feae6c4237034
Pidddd <424f1d@gmail.com>
2025-09-30 14:32:07 +0700

    Sistem Perpustakaan

---
+++ jobsheet5/sistemPerpus.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sistemPerpus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean ktm, registrasi;
+
+        System.out.print("Apakah membawa KTM? (true/false): ");
+        ktm = sc.nextBoolean();
+
+        System.out.print("Apakah sudah registrasi? (true/false): ");
+        registrasi = sc.nextBoolean();
+
+        if (ktm || registrasi) {
+            System.out.println("\nSilahkan Masuk");
+        } else {
+            System.out.println("Dilarang Masuk");
+        }
+
+        sc.close();
+    }
+}

d05b25ca14a3dc6fc0a0617e1b0c630543aeeadd
Pidddd <424f1d@gmail.com>
2025-10-01 15:39:17 +0700

    tugas 1

--- jobsheet5/ifCetakKRS03.java
+++ jobsheet5/ifCetakKRS03.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class ifCetakKRS03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        String pesan;
+
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT Terverifikasi");
-            System.out.println("Silahkan Cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        pesan= (uktLunas) ?"Pembayaran UKT Terverifikasi\nSilahkan Cetak KRS dan minta tanda tangan DPA"
+        :"Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.print(pesan);
         sc.close();
     }
 }

4d7a14075316bbe598f2bc6c15b23ddf4f62974a
Pidddd <424f1d@gmail.com>
2025-10-05 19:05:49 +0700

    Tugas 3

--- jobsheet5/aksesWIFI03.java
+++ jobsheet5/aksesWIFI03.java
@@ -4,25 +4,30 @@ public class aksesWIFI03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String pengguna;
+        int pengguna;
         int sks = 0;
 
-        System.out.print("Masukkan tipe pengguna (Dosen/Mahasiswa): ");
-        pengguna = sc.next();
+        System.out.print("Masukkan tipe pengguna (1=Dosen / 2=Mahasiswa): ");
+        pengguna = sc.nextInt();
 
-        if (pengguna.equalsIgnoreCase("Dosen")) {
-            System.out.println("Akses WIFI Diberikan (Dosen)");
-        } else if (pengguna.equalsIgnoreCase("Mahasiswa")) {
-            System.out.print("Masukkan jumlah SKS: ");
-            sks = sc.nextInt();
+        switch (pengguna) {
+            case 1:
+                System.out.println("Akses WIFI Diberikan (Dosen)");
+                break;
 
-            if (sks >= 12) {
-                System.out.println("Akses WIFI Diberikan (Mahasiswa Aktif)");
-            } else {
-                System.out.println("Akses ditolak, SKS kurang dari 12");
-            }
-        } else {
-            System.out.println("Akses Ditolak");
+            case 2:
+                System.out.print("Masukkan jumlah SKS: ");
+                sks = sc.nextInt();
+
+                if (sks >= 12) {
+                    System.out.println("Akses WIFI Diberikan (Mahasiswa Aktif)");
+                } else {
+                    System.out.println("Akses ditolak, SKS kurang dari 12");
+                }
+                break;
+
+            default:
+                System.out.println("Akses Ditolak (Tipe pengguna tidak dikenal)");
         }
 
         sc.close();

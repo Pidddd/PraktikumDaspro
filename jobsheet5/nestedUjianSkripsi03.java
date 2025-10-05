20e1b5af8ad6b069f03749696138f9b799bba366
Pidddd <424f1d@gmail.com>
2025-10-05 19:08:49 +0700

    Percobaan 3

--- jobsheet5/nestedUjianSkripsi03.java
+++ jobsheet5/nestedUjianSkripsi03.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class nestedUjianSkripsi03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -29,5 +28,6 @@ public class nestedUjianSkripsi03 {
         }
         System.out.println(pesan);
 
+        sc.close();
     }
 }

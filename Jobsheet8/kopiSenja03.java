596c69997101b5dc96fc96e17551fcb1774e2550
Pidddd <424f1d@gmail.com>
2025-11-02 22:42:03 +0700

    Tugas 3

--- Jobsheet8/kopiSenja03.java
+++ Jobsheet8/kopiSenja03.java
@@ -5,34 +5,46 @@ import java.util.Scanner;
 public class kopiSenja03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int jmlCabang = 0, jmlPelanggan = 0, item = 0;
+        int i = 0, j = 0;
+        int totalItemCabang = 0, totalSeluruhPelanggan = 0, totalSeluruhItem = 0;
 
-        int totalItemCabang = 0;
-        int totalSeluruhPelanggan = 0;
-        int totalSeluruhItem = 0;
+        // Input jumlah cabang
+        System.out.print("Jumlah cabang kafe: ");
+        jmlCabang = sc.nextInt();
 
-        System.out.print("Masukkan jumlah cabang: ");
-        int jmlCabang = sc.nextInt();
+        System.out.println("\n=== Input Penjualan Per Cabang ===");
 
-        for (int i = 1; i <= jmlCabang; i++) {
-            System.out.println("\nCabang ke-" + i);
-            System.out.print("Masukkan jumlah pelanggan: ");
-            int jmlPelanggan = sc.nextInt();
+        // Perulangan untuk setiap cabang
+        for (i = 1; i <= jmlCabang; i++) {
+            System.out.println("\n--- Cabang " + i + " ---");
 
-            for (int j = 1; j <= jmlPelanggan; j++) {
-                System.out.print("  Jumlah item yang dipesan pelanggan ke-" + j + ": ");
-                int item = sc.nextInt();
+            System.out.print("Jumlah pelanggan: ");
+            jmlPelanggan = sc.nextInt();
+
+            // Reset total item untuk cabang baru
+            totalItemCabang = 0;
+
+            // Input data setiap pelanggan
+            for (j = 1; j <= jmlPelanggan; j++) {
+                System.out.print("- Pelanggan " + j + " memesan berapa item? ");
+                item = sc.nextInt();
                 totalItemCabang += item;
             }
 
-            System.out.println("Total item terjual di cabang ke-" + i + ": " + totalItemCabang);
+            // Tampilkan hasil per cabang
+            System.out.println("Cabang " + i + ":");
+            System.out.println("- Pelanggan: " + jmlPelanggan + " orang");
+            System.out.println("- Item terjual: " + totalItemCabang);
 
+            // Akumulasi total keseluruhan
             totalSeluruhPelanggan += jmlPelanggan;
             totalSeluruhItem += totalItemCabang;
         }
 
-        System.out.println("\n=== RINGKASAN PENJUALAN ===");
-        System.out.println("Total seluruh pelanggan: " + totalSeluruhPelanggan);
-        System.out.println("Total seluruh item terjual: " + totalSeluruhItem);
+        System.out.println("\nTotal seluruh Cabang:");
+        System.out.println("Pelanggan: " + totalSeluruhPelanggan + " orang");
+        System.out.println("Item terjual: " + totalSeluruhItem + " item");
 
         sc.close();
     }

6e5ab98c138e99b7e43cac11b2b88a47a4b1bf39
Pidddd <424f1d@gmail.com>
2025-11-02 21:57:34 +0700

    Modifikasi Percobaan 4

--- Jobsheet8/NilaiKelompok03.java
+++ Jobsheet8/NilaiKelompok03.java
@@ -8,19 +8,36 @@ public class NilaiKelompok03 {
         int i = 1, nilai;
         float totalNilai = 0, rataNilai;
 
+        // Tambahan variabel untuk mencari nilai tertinggi
+        float rataTertinggi = 0;
+        int kelompokTertinggi = 0;
+
         while (i <= 6) {
             System.out.println("Kelompok " + i);
 
             totalNilai = 0;
             for (int j = 1; j <= 5; j++) {
-                System.out.print(" Nilai dari Kelompok Penilai" + j + ": ");
+                System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
                 nilai = sc.nextInt();
                 totalNilai += nilai;
             }
+
             rataNilai = totalNilai / 5;
-            System.err.println("kelompok " + i + " : nilai rata-rata " + rataNilai);
+            System.out.println("Kelompok " + i + " : nilai rata-rata " + rataNilai);
+
+            // Cek apakah nilai rata-rata saat ini lebih tinggi dari sebelumnya
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             i++;
         }
+
+        System.out.println();
+        System.out.println("Kelompok dengan nilai rata-rata tertinggi adalah Kelompok "
+                + kelompokTertinggi + " dengan rata-rata " + rataTertinggi);
+
         sc.close();
     }
 }

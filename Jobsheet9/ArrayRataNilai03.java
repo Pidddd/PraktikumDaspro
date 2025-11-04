7a6bc3e2440defa05172315aec9d0d3f8e7066d4
Pidddd <424f1d@gmail.com>
2025-11-04 22:43:16 +0700

    Percobaan 3

--- Jobsheet9/ArrayRataNilai03.java
+++ Jobsheet9/ArrayRataNilai03.java
@@ -6,19 +6,42 @@ public class ArrayRataNilai03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0, rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = sc.nextInt();  // input jumlah mahasiswa
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        int[] nilaiMhs = new int[jumlahMhs];
+        double totalNilai = 0;
+        double totalLulus = 0, totalTidakLulus = 0;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
+
+        // Input nilai mahasiswa
+        for (int i = 0; i < jumlahMhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
-        }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+
+            totalNilai += nilaiMhs[i]; // total semua nilai
+
+            // Pisahkan nilai lulus dan tidak lulus
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
+            }
         }
 
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata Nilai = " + rata2);
+        // Hitung rata-rata
+        double rataNilai = totalNilai / jumlahMhs;
+        double rataLulus = (jumlahLulus > 0) ? totalLulus / jumlahLulus : 0;
+        double rataTidakLulus = (jumlahTidakLulus > 0) ? totalTidakLulus / jumlahTidakLulus : 0;
+
+        // Tampilkan hasil
+        System.out.println("Rata-rata nilai keseluruhan = " + rataNilai);
+        System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus = " + jumlahTidakLulus);
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
 
         sc.close();
     }

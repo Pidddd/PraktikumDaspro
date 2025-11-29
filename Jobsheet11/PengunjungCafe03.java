2ef1ecf481af2058557450e97adf0c8491aeb3b8
Pidddd <424f1d@gmail.com>
2025-11-29 21:47:17 +0700

    Modifikasi Percobaan 4

--- Jobsheet11/PengunjungCafe03.java
+++ Jobsheet11/PengunjungCafe03.java
@@ -1,11 +1,10 @@
 package PraktikumDaspro.Jobsheet11;
 
 public class PengunjungCafe03 {
-    static void daftarPengunjung(String... var0) {
+    static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung:");
-
-        for (int var1 = 0; var1 < var0.length; ++var1) {
-            System.out.println("- " + var0[var1]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 

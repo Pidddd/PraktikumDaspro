2b8886d3b5b98eca05bc73d669d3d4233d71cc2e
Pidddd <424f1d@gmail.com>
2025-11-29 21:09:18 +0700

    Modifikasi Percobaan 1

--- Jobsheet11/kafe03.java
+++ Jobsheet11/kafe03.java
@@ -27,11 +27,21 @@ public class kafe03 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Menu("andi", true);
+        System.out.println("Selamat datang, andi!");
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        System.out.println("===== MENU RESTO KAFE ======");
+        System.out.println("1. Kopi Hitam Rp 15,000");
+        System.out.println("2. Cappuccino Rp 20,000");
+        System.out.println("3. Latte Rp 22,000");
+        System.out.println("4. Teh Tarik Rp 12,000");
+        System.out.println("5. Roti Bakar Rp 10,000");
+        System.out.println("6. Mie Goreng Rp 18,000");
+        System.out.println("======== ==");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
 
-        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        System.out.print("Masukkan nomor menu: ");
         int pilihanMenu = sc.nextInt();
-        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        System.out.print("Masukkan jumlah item: ");
         int banyakItem = sc.nextInt();
 
         int totalHarga = hitungTotalHarga03(pilihanMenu, banyakItem);

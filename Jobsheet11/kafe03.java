98049301540f702cf1e607beef4b618dc52c19f6
Pidddd <424f1d@gmail.com>
2025-11-29 21:34:10 +0700

    Modifikasi Percobaan 2

--- Jobsheet11/kafe03.java
+++ Jobsheet11/kafe03.java
@@ -3,11 +3,21 @@ package PraktikumDaspro.Jobsheet11;
 import java.util.Scanner;
 
 public class kafe03 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
+
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
+
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("Kode promo diterima, berikan diskon 50%");
+        } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("Kode promo diterima, berikan diskon 30%");
+        } else {
+            System.out.println("Kode promo invalid");
+        }
+
         System.out.println("===== MENU RESTO KAFE ======");
         System.out.println("1. Kopi Hitam Rp 15,000");
         System.out.println("2. Cappuccino Rp 20,000");
@@ -27,21 +37,11 @@ public class kafe03 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Selamat datang, andi!");
-        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        System.out.println("===== MENU RESTO KAFE ======");
-        System.out.println("1. Kopi Hitam Rp 15,000");
-        System.out.println("2. Cappuccino Rp 20,000");
-        System.out.println("3. Latte Rp 22,000");
-        System.out.println("4. Teh Tarik Rp 12,000");
-        System.out.println("5. Roti Bakar Rp 10,000");
-        System.out.println("6. Mie Goreng Rp 18,000");
-        System.out.println("======== ==");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        Menu("andi", true, "Diskon50");
 
-        System.out.print("Masukkan nomor menu: ");
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
         int pilihanMenu = sc.nextInt();
-        System.out.print("Masukkan jumlah item: ");
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
         int banyakItem = sc.nextInt();
 
         int totalHarga = hitungTotalHarga03(pilihanMenu, banyakItem);
